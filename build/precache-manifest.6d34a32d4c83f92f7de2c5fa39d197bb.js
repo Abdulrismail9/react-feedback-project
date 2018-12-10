4f044d56dcce7e4e6c90f11314883b7cca6b7883
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-12-10 16:28:10 -0600

    heroku

---
+++ build/precache-manifest.6d34a32d4c83f92f7de2c5fa39d197bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74c6b89616a541fb0531",
+    "url": "/static/js/main.74c6b896.chunk.js"
+  },
+  {
+    "revision": "9e7dd3292b55cd2fccb8",
+    "url": "/static/js/1.9e7dd329.chunk.js"
+  },
+  {
+    "revision": "74c6b89616a541fb0531",
+    "url": "/static/css/main.d9e7546c.chunk.css"
+  },
+  {
+    "revision": "02b260204ee2ec39a773aca199ddd73c",
+    "url": "/index.html"
+  }
+];
